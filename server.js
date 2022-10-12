2b035c6fa2593aaa5e7ded12b735769d913b6eef
Dmitry Mokrov <d.mokrov@just-work.org>
2022-10-12 21:13:22 +0300

    Настраивает деплой

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(`${__dirname}/dist`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
